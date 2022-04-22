ab174b88fa397529909ec864c58e428b1e17fbf8
Alok Kushwaha <95715756+thefriendlyprogrammer@users.noreply.github.com>
2022-04-22 14:34:24 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Armstrong_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Armstrong Number");
+            int number, r, sum = 0, temp;
+            Console.WriteLine("Enter the number: ");
+            number = int.Parse(Console.ReadLine());
+            temp = number;
+            while (number > 0)
+            {
+                r = number % 10;
+                sum = sum + (r * r * r);
+                number = number / 10;
+            }
+            if (temp == sum)
+            {
+                Console.WriteLine("Armstong Number");
+            }
+            else
+            {
+                Console.WriteLine("Not Armstrong Number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
